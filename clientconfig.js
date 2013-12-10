88e77d427755126fe81ec01c9742147fea21b832
Peter Henning <petreboy14@gmail.com>
2013-12-10 10:42:33 -0800

    Don't freeze config object

--- clientconfig.js
+++ clientconfig.js
@@ -1,11 +1,6 @@
 var cookies = require('cookie-getter'),
     config = cookies('config') || {};
 
-// freeze it if browser supported
-if (Object.freeze) {
-    Object.freeze(config);
-}
-
 // wipe it out
 document.cookie = 'config=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
 
